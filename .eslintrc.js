29f82596839d86a63a48fe3cf0995df32e45ce8c
Craig Bovis <craig.bovis@inlight.com.au>
2017-08-20 00:46:18 +1000

    Disable eslint prefer-default-export

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-  extends: "airbnb-base",
-  plugins: ["import", "classes"],
+  extends: 'airbnb-base',
+  plugins: ['import', 'classes'],
   globals: { atom: true, document: true },
   rules: {
-    "no-console": "off",
-    "classes/style": 2,
-    "classes/super": 2,
-    "classes/name": [2, "class", "method"],
-    "classes/space": 2
+    'no-console': 'off',
+    'classes/style': 2,
+    'classes/super': 2,
+    'classes/name': [2, 'class', 'method'],
+    'classes/space': 2,
+    'import/prefer-default-export': 'off',
   },
   settings: {
-    "import/core-modules": ["atom"]
-  }
+    'import/core-modules': ['atom'],
+  },
 };
